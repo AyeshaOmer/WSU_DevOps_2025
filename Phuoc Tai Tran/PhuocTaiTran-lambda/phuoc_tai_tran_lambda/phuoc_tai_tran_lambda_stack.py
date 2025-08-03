8939dc6f2a7b784af984aa9c653a86e9f606faf8
Vallad1122 <johnbattern81@gmail.com>
2025-08-03 15:51:24 +1000

    Web status collect function
    collect 3 web status every 15 mins

---
+++ Phuoc Tai Tran/PhuocTaiTran-lambda/phuoc_tai_tran_lambda/phuoc_tai_tran_lambda_stack.py
@@ -0,0 +1,34 @@
+from aws_cdk import (
+    Duration,
+    Stack,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
+    # aws_sqs as sqs,
+    aws_events as events,
+    aws_events_targets as targets,
+)
+from constructs import Construct
+
+class PhuocTaiTranLambdaStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        fn = _lambda.Function( 
+        self, "PhuocTaiTranLambda",
+        runtime=_lambda.Runtime.PYTHON_3_12,
+        handler="index.handler",
+        code = _lambda.Code.from_asset("lib/lambda-handler"),
+        )
+
+        endpoint = apigw.LambdaRestApi(
+        self, "ApiGwEndpoint",
+        handler=fn,
+        rest_api_name="PhuocTaiTranLambdaAPI",
+        )
+
+        rule = events.Rule(
+            self, "ScheduleRule",
+            schedule=events.Schedule.rate(Duration.minutes(15)),
+            targets=[targets.LambdaFunction(fn)]
+        )
