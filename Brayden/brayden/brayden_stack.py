153cea54e9519ae858ffb3cf01a03a84df660a67
Brayden <Braydencoxon13@gmail.com>
2025-08-12 11:48:38 +1000

    Update stack.py as has missing comma

--- Brayden/brayden/brayden_stack.py
+++ Brayden/brayden/brayden_stack.py
@@ -2,7 +2,7 @@ from aws_cdk import (
     # Duration,
     Stack,
     aws_lambda as lambda_,
-    #aws_logs as logs
+    aws_logs as logs, RemovalPolicy
 )
 from constructs import Construct
 
@@ -10,10 +10,10 @@ class BraydenStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-    fn = lambda_.Function(self, "WanMONLambda",
-    runtime=lambda_.Runtime.PYTHON_3_12,
-    handler="WanMONLambda.lambda_handler",
-    code=lambda_.Code.from_asset("./modules")
-    log_removal_policy = Removal.DESTROY
-)
+        
+        fn = lambda_.Function(self, "WanMONLambda",
+            runtime=lambda_.Runtime.PYTHON_3_12,
+            handler="WanMONLambda.lambda_handler",    
+            code=lambda_.Code.from_asset("./modules"),
+            log_removal_policy=RemovalPolicy.DESTROY
+        )   
