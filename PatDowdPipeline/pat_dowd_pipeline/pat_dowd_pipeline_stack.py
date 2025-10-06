99dc2e7f14f4a2e7539d46a1d2e517c483167216
Pat Dowd <pa77y04@gmail.com>
2025-10-07 10:07:45 +1100

    starting stages

--- PatDowdPipeline/pat_dowd_pipeline/pat_dowd_pipeline_stack.py
+++ PatDowdPipeline/pat_dowd_pipeline/pat_dowd_pipeline_stack.py
@@ -12,10 +12,10 @@ class PatDowdPipelineStack(Stack):
         super().__init__(scope, construct_id, **kwargs) 
         # The code that defines your stack goes here 
         source = pipelines.CodePipelineSource.connection(
-            "prinpa/publicTest",
+            "prinpa/WSU_DEVOPS_2025",
             "main",
             connection_arn="arn:aws:codeconnections:ap-southeast-2:457795063739:connection/0067ee37-61b7-473e-a263-f3b975e7b3bf"
         )
         synth = pipelines.ShellStep("synth", input=source, 
-                                    commands=[], ) 
+                                    commands=["npm install -g aws-cdk", "cd PatDowdPipeline", "python -m pip install -r requirements.txt", "cdk synth", "cdk deploy"], ) 
         pipeline = pipelines.CodePipeline(self, "PatPipeline", synth = synth)
