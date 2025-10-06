dbe6a7ee707c3e53d47c3bd51db56f865c3ccc29
Pat Dowd <pa77y04@gmail.com>
2025-10-07 09:28:21 +1100

    updated arn

--- PatDowdPipeline/pat_dowd_pipeline/pat_dowd_pipeline_stack.py
+++ PatDowdPipeline/pat_dowd_pipeline/pat_dowd_pipeline_stack.py
@@ -11,13 +11,11 @@ class PatDowdPipelineStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None: 
         super().__init__(scope, construct_id, **kwargs) 
         # The code that defines your stack goes here 
-        source = pipelines.CodePipelineSource.git_hub( 
-            repo_string = "prinpa/publicTest", 
-            branch = "main", 
-            action_name = "publicTest", 
-            authentication = SecretValue.secrets_manager("pipelineSecret"), 
-            trigger = aws_codepipeline_actions.GitHubTrigger("POLL") 
-        ) 
+        source = pipelines.CodePipelineSource.connection(
+            "prinpa/publicTest",
+            "main",
+            connection_arn="arn:aws:codeconnections:ap-southeast-2:457795063739:connection/0067ee37-61b7-473e-a263-f3b975e7b3bf"
+        )
         synth = pipelines.ShellStep("synth", input=source, 
                                     commands=[], ) 
         pipeline = pipelines.CodePipeline(self, "PatPipeline", synth = synth)
