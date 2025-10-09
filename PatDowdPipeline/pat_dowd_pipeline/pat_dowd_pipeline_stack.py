de524bd1ca69d768c05166eb60480bb4a570b8e9
Pat Dowd <pa77y04@gmail.com>
2025-10-09 13:10:15 +1100

    pipeline stacks

--- PatDowdPipeline/pat_dowd_pipeline/pat_dowd_pipeline_stack.py
+++ PatDowdPipeline/pat_dowd_pipeline/pat_dowd_pipeline_stack.py
@@ -1,21 +1,65 @@
-from aws_cdk import ( 
-  # Duration, 
-  Stack, 
-  pipelines as pipelines, 
-  SecretValue, 
-  aws_codepipeline_actions 
-  # aws_sqs as sqs, 
-) 
-from constructs import Construct 
-class PatDowdPipelineStack(Stack): 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None: 
-        super().__init__(scope, construct_id, **kwargs) 
-        # The code that defines your stack goes here 
+from aws_cdk import (
+    Stack,
+    pipelines as pipelines,
+    aws_iam as iam,
+)
+from constructs import Construct
+
+class PatDowdPipelineStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Source step
         source = pipelines.CodePipelineSource.connection(
             "prinpa/WSU_DEVOPS_2025",
             "main",
-            connection_arn="arn:aws:codeconnections:ap-southeast-2:457795063739:connection/0067ee37-61b7-473e-a263-f3b975e7b3bf"
+            connection_arn="arn:aws:codeconnections:ap-southeast-2:457795063739:connection/0067ee37-61b7-473e-a263-f3b975e7b3bf",
         )
-        synth = pipelines.ShellStep("synth", input=source, 
-                                    commands=["npm install -g aws-cdk", "cd PatDowdPipeline", "python -m pip install -r requirements.txt", "cdk synth", "cdk deploy"], ) 
-        pipeline = pipelines.CodePipeline(self, "PatPipeline", synth = synth)
+
+        # Synth step
+        synth = pipelines.ShellStep(
+            "Synth",
+            input=source,
+            commands=[
+                "npm install -g aws-cdk",
+                "cdk bootstrap",
+                "cd PatDowdPipeline",
+                "python -m pip install -r requirements.txt",
+                "cdk synth"
+            ],
+            primary_output_directory="PatDowdPipeline/cdk.out"
+        )
+
+        # Unit test step
+        unit_test = pipelines.ShellStep(
+            "UnitTests",
+            commands=[
+                "cd PatDowdPipeline",
+                "python -m pip install -r requirements-dev.txt",
+                "pytest"
+            ]
+        )
+
+        # Define IAM role for the pipeline
+        pipeline_role = iam.Role(
+            self,
+            "PipelineRole",
+            assumed_by=iam.ServicePrincipal("codepipeline.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("AWSCodePipeline_FullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AWSCodeBuildDeveloperAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AWSCodeCommitPowerUser"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AWSCloudFormationFullAccess"),
+            ],
+        )
+
+        # Create pipeline with the role
+        pipeline = pipelines.CodePipeline(
+        self,
+        "PatPipeline",
+        synth=synth,
+        role=pipeline_role
+        )
+        # Add unit test step
+        pipeline.add_wave("TestWave", pre=[unit_test])
--- PatDowdPipeline/tests/unit/test_pat_dowd_pipeline_stack.py
+++ PatDowdPipeline/tests/unit/test_pat_dowd_pipeline_stack.py
@@ -1,15 +1,26 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
-
+import pytest
 from pat_dowd_pipeline.pat_dowd_pipeline_stack import PatDowdPipelineStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in pat_dowd_pipeline/pat_dowd_pipeline_stack.py
-def test_sqs_queue_created():
+
+@pytest.fixtures
+def getStack():
     app = core.App()
     stack = PatDowdPipelineStack(app, "pat-dowd-pipeline")
-    template = assertions.Template.from_stack(stack)
+    return stack
+
+def test_sqs_queue_created(getStack):
+    template = assertions.Template.from_stack(getStack)
+
+    template.resource_count_in("AWS::Lmabda::Function", 2)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
 #     })
+def test_lambda(getStack):
+    template = assertions.Template.from_stack(getStack)
+    template.resource_count_in("AWS::Lmabda::Function", 2)
+
