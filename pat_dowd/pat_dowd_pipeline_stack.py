6256ec6eb217d8e2108fd842b3c1559b4ceda783
Pat Dowd <pa77y04@gmail.com>
2025-10-13 09:47:14 +1100

    adding a venv

--- pat_dowd/pat_dowd_pipeline_stack.py
+++ pat_dowd/pat_dowd_pipeline_stack.py
@@ -37,6 +37,7 @@ class PatDowdPipelineStack(Stack):
         unit_test = pipelines.ShellStep(
             "unitTest",
             commands=[
+                "python -m venv .venv", 
                 "source .venv/bin/activate",
                 "python -m pip install aws-cdk-lib", 
                 "python -m pip install -r requirements-dev.txt",
