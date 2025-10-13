a7639d126ffa35d029dc48f0addc871ee14e5bdf
Pat Dowd <pa77y04@gmail.com>
2025-10-13 14:59:41 +1100

    testing waves

--- pat_dowd/pat_dowd_pipeline_stack.py
+++ pat_dowd/pat_dowd_pipeline_stack.py
@@ -79,21 +79,21 @@ class PatDowdPipelineStack(Stack):
 
         # Test stage (no deployment)
         test_stage = TestStage(self, "Test")
-        pipeline.add_stage(
+        pipeline.add_wave(
             test_stage,
             pre=[unit_test]
         )
 
         # Integration test stage (no deployment)
-        integration_stage = TestStage(self, "Integration")
-        pipeline.add_stage(
-            integration_stage,
-            pre=[integration_test]
-        )
+        # integration_stage = TestStage(self, "Integration")
+        # pipeline.add_stage(
+        #     integration_stage,
+        #     pre=[integration_test]
+        # )
 
         # Final stage with actual deployment and production tests
-        prod_stage = MypipelineStage(self, "Production")
-        pipeline.add_stage(
-            prod_stage,
-            pre=[prod_test]
-        )
+        # prod_stage = MypipelineStage(self, "Production")
+        # pipeline.add_stage(
+        #     prod_stage,
+        #     pre=[prod_test]
+        # )
