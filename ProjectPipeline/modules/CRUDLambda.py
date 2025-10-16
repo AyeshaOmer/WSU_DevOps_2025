7348fcdde3b7a82b153f3dc99ab44aceb5d67ca9
EugeneKosiak <22067815@student.westernsydney.edu.au>
2025-10-16 12:08:58 +1100

    Possible fix for resource not found

--- ProjectPipeline/modules/CRUDLambda.py
+++ ProjectPipeline/modules/CRUDLambda.py
@@ -1,9 +1,10 @@
 import json
 import boto3
+import os
 from boto3.dynamodb.conditions import Key
 
-dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table("TargetListTable")
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['TABLE_NAME'])
 
 def lambda_handler(event, context):
     http_method = event.get("httpMethod")
