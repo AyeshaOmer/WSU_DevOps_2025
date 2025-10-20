ec1c4e14a5c99a3a72e8ba39bf4822af0c14778a
EugeneKosiak <22067815@student.westernsydney.edu.au>
2025-10-20 11:52:02 +1100

    Add prod stage

--- ProjectPipeline/project_pipeline/project_pipeline_stack.py
+++ ProjectPipeline/project_pipeline/project_pipeline_stack.py
@@ -122,7 +122,12 @@ class ProjectPipelineStack(Stack):
         '''
         # https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.pipelines/ManualApprovalStep.html
         # implement after pipeline dployment is fixed
-        pre=[pipelines.ManualApprovalStep("PromoteToProd",)]
+        # pre=[pipelines.ManualApprovalStep("PromoteToProd",)]
+        prod = MyAppStage(self, 'Prod')
+        WHpipeline.add_stage(
+            prod,
+            pre=[pipelines.ManualApprovalStep("PromoteToProd")]
+        )
 
         
 
